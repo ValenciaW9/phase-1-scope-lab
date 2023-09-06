8a67e514717f8d8aa7c58203e3c20d0e3f1d8d0d
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-06 12:59:32 -0400

    phase-1-scope-lab/
    phase-1-scope-lab/

--- index.js
+++ index.js
@@ -9,47 +9,8 @@
  //When upperCaseCustomerName is called,it converts the customerName to uppercase and then invokes the callback function passed as an argument , in this case, greetCustomer.
 //it will first convert the customerName to uppercase and then log the greeting message with the updated name.
 
-
 var customerName = 'bob';
 
-function upperCaseCustomerName() {
-    customerName = customerName.toUpperCase();
-}
-
-upperCaseCustomerName(); // Call the function to convert customerName to uppercase
-Fixing the setBestCustomer function:
-var bestCustomer; // Declare the bestCustomer variable in global scope
-
-function setBestCustomer() {
-    bestCustomer = 'not bob';
-}
-
-setBestCustomer(); // Call the function to assign the value 'not bob' to bestCustomer
-Fixing the overWriteBestCustomer function:
-function overWriteBestCustomer() {
-    bestCustomer = 'maybe bob'; // Assuming bestCustomer is already declared in global scope
-}
-
-overWriteBestCustomer(); // Call the function to overwrite the value of bestCustomer
-Fixing the changeLeastFavoriteCustomer function:
-const leastFavoriteCustomer = 'bob';
-
-function changeLeastFavoriteCustomer() {
-    // Attempting to change the value of a constant will throw an error
-    // You cannot change the value of leastFavoriteCustomer
-    // You can remove this function or modify it to do something else
-    console.log('Cannot change the value of leastFavoriteCustomer');
-}
-
-changeLeastFavoriteCustomer(); // Call the function (optional)
-//Since leastFavoriteCustomer is a constant, you cannot change its value. If you want to modify it, you need to declare it as a variable using `let` or `var` instead of `const`.
-
-// the greetCustomer function and callback:
-
-//javascript///
-
- var customerName = 'bob';
- 
 function upperCaseCustomerName(callback) {
     customerName = customerName.toUpperCase();
     callback();
@@ -61,22 +22,38 @@ function greetCustomer() {
 
 upperCaseCustomerName(greetCustomer); // Call the upperCaseCustomerName function passing greetCustomer as a callback
 
-
 // In the beginning of your code, declare customerName in global scope
 var customerName = 'bob';
- setBestCustomer() 
-// Before the setBestCustomer function, declare bestCustomer in global scope
+
+// Declare the bestCustomer variable in global scope
 var bestCustomer;
 
-//  setBestCustomer function is already correct, no need to modify it
- changeLeastFavoriteCustomer() 
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+setBestCustomer(); // Call the function to assign the value 'not bob' to bestCustomer
+
 // Before the changeLeastFavoriteCustomer function, declare leastFavoriteCustomer in global scope
 const leastFavoriteCustomer = 'bob';
 
-//  changeLeastFavoriteCustomer function is already correct, no need to modify it
- upperCaseCustomerName() 
+function changeLeastFavoriteCustomer() {
+    console.log('Cannot change the value of leastFavoriteCustomer');
+}
+
+changeLeastFavoriteCustomer(); // Call the function (optional)
+
 // At the end of the upperCaseCustomerName function, add a return statement to return the customerName
-return customerName;
- overwriteBestCustomer() 
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
 // Before the overwriteBestCustomer function, declare bestCustomer in global scope
 var bestCustomer;
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+overwriteBestCustomer(); // Call the function to overwrite the value of bestCustomer
