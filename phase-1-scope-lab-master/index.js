568d5a0f8981676be1f4d328429aef81bdd05dbf
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-06 13:12:58 -0400

    phase-1-scope-lab/
    phase-1-scope-lab/

--- phase-1-scope-lab-master/index.js
+++ phase-1-scope-lab-master/index.js
@@ -9,7 +9,11 @@
  //When upperCaseCustomerName is called,it converts the customerName to uppercase and then invokes the callback function passed as an argument , in this case, greetCustomer.
 //it will first convert the customerName to uppercase and then log the greeting message with the updated name.
 
+// Declare customerName in global scope
 var customerName = 'bob';
+
+
+//Rest of the code...
 
 function upperCaseCustomerName(callback) {
     customerName = customerName.toUpperCase();
@@ -24,7 +28,6 @@ upperCaseCustomerName(greetCustomer); // Call the upperCaseCustomerName function
 
 // In the beginning of your code, declare customerName in global scope
 var customerName = 'bob';
-
 // Declare the bestCustomer variable in global scope
 var bestCustomer;
 
@@ -34,14 +37,21 @@ function setBestCustomer() {
 
 setBestCustomer(); // Call the function to assign the value 'not bob' to bestCustomer
 
-// Before the changeLeastFavoriteCustomer function, declare leastFavoriteCustomer in global scope
+
+// Declare the leastFavoriteCustomer variable in global scope
 const leastFavoriteCustomer = 'bob';
 
 function changeLeastFavoriteCustomer() {
-    console.log('Cannot change the value of leastFavoriteCustomer');
+    throw new Error('Cannot change the value of leastFavoriteCustomer');
 }
 
-changeLeastFavoriteCustomer(); // Call the function (optional)
+// Wrap the call to changeLeastFavoriteCustomer in a try-catch block
+try {
+    changeLeastFavoriteCustomer(); // Call the function (optional)
+} catch (error) {
+    console.log(error.message);
+}
+
 
 // At the end of the upperCaseCustomerName function, add a return statement to return the customerName
 function upperCaseCustomerName() {
@@ -49,7 +59,7 @@ function upperCaseCustomerName() {
     return customerName;
 }
 
-// Before the overwriteBestCustomer function, declare bestCustomer in global scope
+// Declare the bestCustomer variable in global scope
 var bestCustomer;
 
 function overwriteBestCustomer() {
