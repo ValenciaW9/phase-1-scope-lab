19fce7387e763e7d261b9dee9bb53385d60346f2
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-06 13:18:47 -0400

    phase-1-scope-lab/
    phase-1-scope-lab/

--- phase-1-scope-lab-master/index.js
+++ phase-1-scope-lab-master/index.js
@@ -8,26 +8,8 @@
  //we added a new function called greetCustomeer which logs a new greeting messsage with the customer's name . //We then modified the upperCaseCustomerName function to accept a callback function as a argument.
  //When upperCaseCustomerName is called,it converts the customerName to uppercase and then invokes the callback function passed as an argument , in this case, greetCustomer.
 //it will first convert the customerName to uppercase and then log the greeting message with the updated name.
-
 // Declare customerName in global scope
-var customerName = 'bob';
 
-
-//Rest of the code...
-
-function upperCaseCustomerName(callback) {
-    customerName = customerName.toUpperCase();
-    callback();
-}
-
-function greetCustomer() {
-    console.log('Hello ' + customerName + '!');
-}
-
-upperCaseCustomerName(greetCustomer); // Call the upperCaseCustomerName function passing greetCustomer as a callback
-
-// In the beginning of your code, declare customerName in global scope
-var customerName = 'bob';
 // Declare the bestCustomer variable in global scope
 var bestCustomer;
 
@@ -37,12 +19,11 @@ function setBestCustomer() {
 
 setBestCustomer(); // Call the function to assign the value 'not bob' to bestCustomer
 
-
 // Declare the leastFavoriteCustomer variable in global scope
 const leastFavoriteCustomer = 'bob';
 
 function changeLeastFavoriteCustomer() {
-    throw new Error('Cannot change the value of leastFavoriteCustomer');
+    throw new Error('Assignment to constant variable.');
 }
 
 // Wrap the call to changeLeastFavoriteCustomer in a try-catch block
@@ -50,15 +31,12 @@ try {
     changeLeastFavoriteCustomer(); // Call the function (optional)
 } catch (error) {
     console.log(error.message);
-}
 
-
-// At the end of the upperCaseCustomerName function, add a return statement to return the customerName
+    // At the end of the upperCaseCustomerName function, add a return statement to return the customerName
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase();
     return customerName;
 }
-
 // Declare the bestCustomer variable in global scope
 var bestCustomer;
 
